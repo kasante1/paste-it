2b82609c82b8e24de6996a3a5aed5c14008d45bd
kasante1 <verbsgh@gmail.com>
2024-05-08 02:56:58 +0000

    feat: add env access

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,33 @@ package main
 import (
 "log"
 "net/http"
+"flag"
+"os" 
 )
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime| log.Llongfile)
+
 	mux := http.NewServeMux()
+
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:
+		 *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+		}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
