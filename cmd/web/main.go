619bb55ddb20e6eb0ca46fab5124d236c8cfac32
kasante1 <verbsgh@gmail.com>
2024-05-08 14:07:01 +0000

    chore: setup db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,13 @@
 package main
-import (
-"log"
-"net/http"
-"flag"
-"os" 
-)
 
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	_ "github.com/go-sql-driver/mysql" 
+)
 
 type application struct {
 	errorLog *log.Logger
@@ -13,28 +15,51 @@ type application struct {
 
 }
 
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+	return nil, err
+	}
+	if err = db.Ping(); err != nil {
+	return nil, err
+	}
+	return db, nil
+	}
+
+
 func main() {
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	dsn := flag.String("dsn", "k:Strong234?@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime| log.Llongfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+		if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
 		infoLog:
-		 infoLog,
-		}
-
-
+		infoLog,
+	}
 
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
 		Handler: app.routes(),
-		}
-
+	}
+	
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
